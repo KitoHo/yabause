e940665ea1651c4ca691e42376fa45e0f591b08d
cyberwarriorx <cyberwarriorx@d7be59e3-070e-4b9a-85a1-4646befae34d>
2005-07-16 01:50:42 +0000

    -Added VDP2 framework. It's obviously incomplete at this point.

---
+++ yabause/src/vdp2.c
@@ -0,0 +1,176 @@
+/*  Copyright 2003-2005 Guillaume Duhamel
+    Copyright 2004-2005 Theo Berkau
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <stdlib.h>
+#include "vdp2.h"
+#include "sh2core.h"
+#include "yabause.h"
+
+u8 * Vdp2Ram;
+Vdp2 * Vdp2Regs;
+
+//////////////////////////////////////////////////////////////////////////////
+
+u8 FASTCALL Vdp2RamReadByte(u32 addr) {
+   return T1ReadByte(Vdp2Ram, addr);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+u16 FASTCALL Vdp2RamReadWord(u32 addr) {
+   return T1ReadWord(Vdp2Ram, addr);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+u32 FASTCALL Vdp2RamReadLong(u32 addr) {
+   return T1ReadLong(Vdp2Ram, addr);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2RamWriteByte(u32 addr, u8 val) {
+   T1WriteByte(Vdp2Ram, addr, val);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2RamWriteWord(u32 addr, u16 val) {
+   T1WriteWord(Vdp2Ram, addr, val);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2RamWriteLong(u32 addr, u32 val) {
+   T1WriteLong(Vdp2Ram, addr, val);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+int Vdp2Init(int coreid) {
+   if ((Vdp2Regs = (Vdp2 *) calloc(1, sizeof(Vdp2))) == NULL)
+      return -1;
+
+   Vdp2Reset();
+   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2DeInit(void) {
+   if (Vdp2Regs)
+      free(Vdp2Regs);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2Reset(void) {
+   Vdp2Regs->TVMD = 0x0000;
+   Vdp2Regs->EXTEN = 0x0000;
+   Vdp2Regs->TVSTAT = Vdp2Regs->TVSTAT & 0x1;
+   Vdp2Regs->RAMCTL = 0x0000;
+   Vdp2Regs->BGON = 0x0000;
+   Vdp2Regs->CHCTLA = 0x0000;
+   Vdp2Regs->CHCTLB = 0x0000;
+   Vdp2Regs->BMPNA = 0x0000;
+   Vdp2Regs->MPOFN = 0x0000;
+   Vdp2Regs->MPABN2 = 0x0000;
+   Vdp2Regs->MPCDN2 = 0x0000;
+   Vdp2Regs->BKTAU = 0x0000;
+   Vdp2Regs->BKTAL = 0x0000;
+   Vdp2Regs->CRAOFA = 0x0000;
+   Vdp2Regs->CRAOFB = 0x0000;
+   Vdp2Regs->PRINA = 0x0000;
+   Vdp2Regs->PRINB = 0x0000;
+   Vdp2Regs->PRIR = 0x0000;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2VBlankIN(void) {
+   Vdp2Regs->TVSTAT |= 0x0008;
+//        satmem->scu->sendVBlankIN();
+
+   if (yabsys.IsSSH2Running)
+      SH2SendInterrupt(SSH2, 0x6, 0x43);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2HBlankIN(void) {
+   Vdp2Regs->TVSTAT |= 0x0004;
+//        satmem->scu->sendHBlankIN();
+
+   if (yabsys.IsSSH2Running)
+      SH2SendInterrupt(SSH2, 0x2, 0x41);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2HBlankOUT(void) {
+   Vdp2Regs->TVSTAT &= ~0x0004;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void Vdp2VBlankOUT(void) {
+   Vdp2Regs->TVSTAT = (Vdp2Regs->TVSTAT & ~0x0008) | 0x0002;
+
+  if (Vdp2Regs->TVMD & 0x8000) {
+     // draw here
+  }
+
+//  satmem->scu->sendVBlankOUT();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+u8 FASTCALL Vdp2ReadByte(u32 addr) {
+   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+u16 FASTCALL Vdp2ReadWord(u32 addr) {
+   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+u32 FASTCALL Vdp2ReadLong(u32 addr) {
+   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2WriteByte(u32 addr, u8 val) {
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2WriteWord(u32 addr, u16 val) {
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void FASTCALL Vdp2WriteLong(u32 addr, u32 val) {
+}
+
+//////////////////////////////////////////////////////////////////////////////

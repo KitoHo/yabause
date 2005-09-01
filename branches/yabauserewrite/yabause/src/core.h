84c51a8305a197cfc455f986c27f96f0448589e0
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2005-09-01 16:05:10 +0000

    fixes for the 64 bits port. can boot the bios now... still crash after radiant silvergun start screen though

--- branches/yabauserewrite/yabause/src/core.h
+++ branches/yabauserewrite/yabause/src/core.h
@@ -17,6 +17,7 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long u64;
+typedef unsigned long pointer;
 
 typedef signed char s8;
 typedef signed short s16;
@@ -27,6 +28,7 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned long u32;
 typedef unsigned long long u64;
+typedef unsigned long pointer;
 
 typedef signed char s8;
 typedef signed short s16;

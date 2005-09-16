6263476acb01dcac286c9d414ab929a1575b6967
cyberwarriorx <cyberwarriorx@d7be59e3-070e-4b9a-85a1-4646befae34d>
2005-09-16 01:05:20 +0000

    -YuiErrorMsg changed to accommodate the new error handling system

--- yabause/src/linux/yui.c
+++ yabause/src/linux/yui.c
@@ -256,21 +256,9 @@ int YuiInit(void) {
    return 0;
 }
 
-void YuiErrorMsg(const char *error_text, SH2_struct *sh2opcodes) {
-   fprintf(stderr, "Error: %s\n", error_text);
-
-   fprintf(stderr, "R0 = %08X\tR12 = %08X\n", sh2opcodes->regs.R[0], sh2opcodes->regs.R[12]);
-   fprintf(stderr, "R1 = %08X\tR13 = %08X\n", sh2opcodes->regs.R[1], sh2opcodes->regs.R[13]);
-   fprintf(stderr, "R2 = %08X\tR14 = %08X\n", sh2opcodes->regs.R[2], sh2opcodes->regs.R[14]);
-   fprintf(stderr, "R3 = %08X\tR15 = %08X\n", sh2opcodes->regs.R[3], sh2opcodes->regs.R[15]);
-   fprintf(stderr, "R4 = %08X\tSR = %08X\n", sh2opcodes->regs.R[4], sh2opcodes->regs.SR.all);
-   fprintf(stderr, "R5 = %08X\tGBR = %08X\n", sh2opcodes->regs.R[5], sh2opcodes->regs.GBR);
-   fprintf(stderr, "R6 = %08X\tVBR = %08X\n", sh2opcodes->regs.R[6], sh2opcodes->regs.VBR);
-   fprintf(stderr, "R7 = %08X\tMACH = %08X\n", sh2opcodes->regs.R[7], sh2opcodes->regs.MACH);
-   fprintf(stderr, "R8 = %08X\tMACL = %08X\n", sh2opcodes->regs.R[8], sh2opcodes->regs.MACL);
-   fprintf(stderr, "R9 = %08X\tPR = %08X\n", sh2opcodes->regs.R[9], sh2opcodes->regs.PR);
-   fprintf(stderr, "R10 = %08X\tPC = %08X\n", sh2opcodes->regs.R[10], sh2opcodes->regs.PC);
-   fprintf(stderr, "R11 = %08X\n", sh2opcodes->regs.R[11]);
+void YuiErrorMsg(const char *string)
+{
+   fprintf(stderr, string);
 }
 
 enum {

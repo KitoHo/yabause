b29ae330c11278174e0c66368be504c240dce814
nobody <nobody@d7be59e3-070e-4b9a-85a1-4646befae34d>
2005-09-27 20:51:08 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_5_0'.

---
+++ tags/release_0_5_0/yabause/src/windows/resource.h
@@ -0,0 +1,120 @@
+/*  Copyright 2004 Theo Berkau
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#define IDR_MENU        10000
+#define IDI_ICON        10001
+#define IDM_CHOOSEBIOS  10002
+#define IDM_CHOOSECDROM 10003
+#define IDM_MEMTRANSFER 10004
+#define IDM_RUN         10005
+#define IDM_PAUSE       10006
+#define IDM_SETTINGS    10007
+#define IDM_EXIT        10008
+#define IDM_MSH2DEBUG   10009
+#define IDM_SSH2DEBUG   10010
+#define IDM_VDP2DEBUG   10011
+#define IDM_M68KDEBUG   10012
+#define IDM_SCUDSPDEBUG 10013
+
+#define IDC_EDITTEXT1                   1001
+#define IDC_EDITTEXT2                   1002
+#define IDC_EDITTEXT3                   1003
+
+#define IDC_LISTBOX1                    1004
+#define IDC_LISTBOX2                    1005
+#define IDC_LISTBOX3                    1006
+#define IDC_LISTBOX4                    1007
+
+#define IDC_CHECKBOX1                   1008
+#define IDC_CHECKBOX2                   1009
+#define IDC_CHECKBOX3                   1010
+#define IDC_CHECKBOX4                   1011
+#define IDC_CHECKBOX5                   1012
+                                            
+#define IDC_STATUSBAR                   1020
+
+#define IDC_STEP                        1025
+#define IDC_STEPOVER                    1026
+#define IDC_MEMTRANSFER                 1027
+
+#define IDC_ADDBP1                      1028
+#define IDC_DELBP1                      1029
+
+#define IDC_ADDBP2                      1030
+#define IDC_DELBP2                      1031
+
+#define IDC_CHKREAD                     1032
+#define IDC_CHKWRITE                    1033
+#define IDC_CHKBYTE                     1034
+#define IDC_CHKWORD                     1035
+#define IDC_CHKDWORD                    1036
+
+#define IDC_BROWSE                      1037
+#define IDC_DOWNLOADMEM                 1038
+#define IDC_UPLOADMEM                   1039
+
+//////////////////////////////////////////////////////////////////////////////
+// Settings stuff
+
+#define IDC_DISCTYPECB                  1001
+#define IDC_DRIVELETTERCB               1002
+#define IDC_IMAGEEDIT                   1003
+#define IDC_IMAGEBROWSE                 1004
+
+#define IDC_BIOSLANGCB                  1005
+
+#define IDC_REGIONCB                    1006
+
+#define IDC_BIOSEDIT                    1007
+#define IDC_BIOSBROWSE                  1008
+
+#define IDC_BACKUPRAMEDIT               1009
+#define IDC_BACKUPRAMBROWSE             1010
+
+#define IDC_MPEGROMEDIT                 1011
+#define IDC_MPEGROMBROWSE               1012
+
+#define IDC_CARTTYPECB                  1013
+#define IDC_CARTEDIT                    1014
+#define IDC_CARTBROWSE                  1015
+
+#define IDC_VIDEOSETTINGS               1016
+#define IDC_SOUNDSETTINGS               1017
+#define IDC_INPUTSETTINGS               1018
+
+//////////////////////////////////////////////////////////////////////////////
+// VDP2 debug stuff
+
+#define IDC_NBG0ENABCB                  1001
+#define IDC_NBG0ET                      1002
+
+#define IDC_NBG1ENABCB                  1003
+#define IDC_NBG1ET                      1004
+
+#define IDC_NBG2ENABCB                  1005
+#define IDC_NBG2ET                      1006
+
+#define IDC_NBG3ENABCB                  1007
+#define IDC_NBG3ET                      1008
+
+#define IDC_RBG0ENABCB                  1009
+#define IDC_RBG0ET                      1010
+
+//////////////////////////////////////////////////////////////////////////////
+

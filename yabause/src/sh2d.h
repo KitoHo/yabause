b193af1bba06f56d00aa5736f586b70509728398
brainnolo <brainnolo@d7be59e3-070e-4b9a-85a1-4646befae34d>
2006-04-04 21:35:07 +0000

    Import of new Mac OS X Port

--- yabause/src/sh2d.h
+++
@@ -1,8 +0,0 @@
-#ifndef SH2D_H
-#define SH2D_H
-
-#include "core.h"
-
-void SH2Disasm(u32 v_addr, u16 op, int mode, char *string);
-
-#endif
--- yabause/src/sh2idle.h
+++
@@ -1,7 +0,0 @@
-#ifndef SH2IDLE_H
-#define SH2IDLE_H
-
-void FASTCALL SH2idleCheck(SH2_struct *context, u32 cycles);
-void FASTCALL SH2idleParse(SH2_struct *context, u32 cycles);
-
-#endif

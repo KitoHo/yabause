3cf366ca5894f271e5b5d7a50ced3623cf89f3e2
nobody <nobody@d7be59e3-070e-4b9a-85a1-4646befae34d>
2007-06-24 17:12:42 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_8_5'.

---
+++ tags/release_0_8_5/yabause/src/gtk/gtkglwidget.c
@@ -0,0 +1,204 @@
+/*  Copyright 2006 Guillaume Duhamel
+    Copyright 2006 Fabien Coulon
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "gtkglwidget.h"
+#ifdef HAVE_LIBGL
+#include <gtk/gtkgl.h>
+#endif
+#include "../vidsoft.h"
+
+#define X_NOSCALE 160
+#define Y_NOSCALE 120
+
+static void yui_gl_class_init	(YuiGlClass * klass);
+static void yui_gl_init		(YuiGl      * yfe);
+static gboolean yui_gl_resize   (GtkWidget *w,GdkEventConfigure *event, gpointer data);
+
+int yui_gl_draw(YuiGl * glxarea) {
+#ifdef HAVE_LIBGL
+	GdkGLContext *glcontext = gtk_widget_get_gl_context (GTK_WIDGET(glxarea));
+	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (GTK_WIDGET(glxarea));
+
+	if (!gdk_gl_drawable_make_current (gldrawable, glcontext)) {
+		g_print("Cannot set gl drawable current\n");
+		return;
+	}
+
+	gdk_gl_drawable_swap_buffers(gldrawable);
+#else
+	int buf_width, buf_height;
+	GdkPixbuf * pixbuf, * scaledpixbuf;
+	GdkRectangle rect = { 0, 0, GTK_WIDGET(glxarea)->allocation.width, GTK_WIDGET(glxarea)->allocation.height };
+	
+	VIDSoftGetScreenSize( &buf_width, &buf_height );
+	glxarea->pixels_width = GTK_WIDGET(glxarea)->allocation.width;
+	glxarea->pixels_height = GTK_WIDGET(glxarea)->allocation.height;
+	glxarea->pixels_rowstride = glxarea->pixels_width * 4;
+	glxarea->pixels_rowstride += (glxarea->pixels_rowstride % 4)? (4 - (glxarea->pixels_rowstride % 4)): 0;
+
+	pixbuf = gdk_pixbuf_new_from_data(dispbuffer, GDK_COLORSPACE_RGB, TRUE, 8,
+			buf_width, buf_height, buf_width*4, NULL, NULL);
+
+	if (( glxarea->pixels_width < buf_width + X_NOSCALE )&&( glxarea->pixels_height < buf_height + Y_NOSCALE )) {
+
+	  gdk_draw_pixbuf(GTK_WIDGET(glxarea)->window, NULL, pixbuf, 0, 0,
+			  (glxarea->pixels_width-buf_width)/2, (glxarea->pixels_height-buf_height)/2,
+			  buf_width, buf_height, GDK_RGB_DITHER_NONE, 0, 0);
+	} else {
+	  
+	  scaledpixbuf = gdk_pixbuf_scale_simple(pixbuf, 
+						 glxarea->pixels_width, glxarea->pixels_height, GDK_INTERP_NEAREST );
+	  gdk_draw_pixbuf(GTK_WIDGET(glxarea)->window, NULL, 
+			  scaledpixbuf, 0, 0, 0, 0, glxarea->pixels_width, glxarea->pixels_height, 
+			  GDK_RGB_DITHER_NONE, 0, 0);
+	  g_object_unref(scaledpixbuf);
+	}
+	g_object_unref(pixbuf);
+#endif
+	glxarea->is_init = 1;
+}
+
+int yui_gl_draw_pause(YuiGl * glxarea) {
+#ifdef HAVE_LIBGL
+	if (glxarea->pixels) {
+		/* The "correct" raster position would be (0, height) but it's not a
+		 * valid position, so I have to use this hack... found here:
+		 * http://www.opengl.org/resources/features/KilgardTechniques/oglpitfall/ */ 
+		glRasterPos2i(0, 0);
+		glBitmap(0, 0, 0, 0, 0, - glxarea->pixels_height, NULL);
+		glPixelZoom(1, 1);
+		glDrawPixels(glxarea->pixels_width, glxarea->pixels_height, GL_RGB, GL_UNSIGNED_BYTE, glxarea->pixels);
+		yui_gl_draw(glxarea);
+	} else {
+		gdk_draw_rectangle(GTK_WIDGET(glxarea)->window, GTK_WIDGET(glxarea)->style->bg_gc[GTK_WIDGET_STATE (glxarea)],
+				TRUE, 0, 0, GTK_WIDGET(glxarea)->allocation.width, GTK_WIDGET(glxarea)->allocation.height);
+	}
+#else
+	if (dispbuffer)
+		yui_gl_draw(glxarea);
+#endif
+}
+
+static gboolean yui_gl_resize(GtkWidget *w,GdkEventConfigure *event, gpointer data) {
+#ifdef HAVE_LIBGL
+	GdkGLContext *glcontext = gtk_widget_get_gl_context (w);
+	GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (w);
+
+	if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext))
+		return FALSE;
+
+	glViewport(0, 0, event->width, event->height);
+        if ( YUI_GL(w)->is_init ) VIDCore->Resize(event->width, event->height, FALSE );
+#endif
+	return FALSE;
+}
+
+GtkWidget * yui_gl_new(void) {
+	GtkWidget * drawingArea;
+#ifdef HAVE_LIBGL
+	int attribs[] = {
+		GDK_GL_RGBA,
+		GDK_GL_RED_SIZE,   1,
+		GDK_GL_GREEN_SIZE, 1,
+		GDK_GL_BLUE_SIZE,  1,
+
+		GDK_GL_DOUBLEBUFFER,
+
+		GDK_GL_DEPTH_SIZE ,1,
+
+		GDK_GL_ATTRIB_LIST_NONE 
+	};
+#endif
+
+	drawingArea = GTK_WIDGET(g_object_new(yui_gl_get_type(), NULL));
+	YUI_GL(drawingArea)->is_init = 0;
+
+#ifdef HAVE_LIBGL
+	gtk_widget_set_gl_capability(drawingArea, gdk_gl_config_new(attribs), NULL, TRUE, GDK_GL_RGBA_TYPE);
+#endif
+
+	g_signal_connect (GTK_OBJECT(drawingArea),"configure_event", GTK_SIGNAL_FUNC(yui_gl_resize),0);
+
+	return drawingArea;
+}
+
+void yui_gl_dump_screen(YuiGl * glxarea) {
+#ifdef HAVE_LIBGL
+	int size;
+
+	glxarea->pixels_width = GTK_WIDGET(glxarea)->allocation.width;
+	glxarea->pixels_height = GTK_WIDGET(glxarea)->allocation.height;
+	glxarea->pixels_rowstride = glxarea->pixels_width * 3;
+	glxarea->pixels_rowstride += (glxarea->pixels_rowstride % 4)? (4 - (glxarea->pixels_rowstride % 4)): 0;
+
+        size = glxarea->pixels_rowstride * glxarea->pixels_height;
+ 
+	if (glxarea->pixels) free(glxarea->pixels);
+        glxarea->pixels = malloc(sizeof(GLubyte) * size);
+        if (glxarea->pixels == NULL) return;    
+
+        glReadPixels(0, 0, glxarea->pixels_width, glxarea->pixels_height, GL_RGB, GL_UNSIGNED_BYTE, glxarea->pixels);
+#endif
+}
+
+void yui_gl_screenshot(YuiGl * glxarea) {
+	GdkPixbuf * pixbuf, * correct;
+       
+	yui_gl_dump_screen(glxarea);
+	pixbuf = gdk_pixbuf_new_from_data(glxarea->pixels, GDK_COLORSPACE_RGB, FALSE, 8,
+			glxarea->pixels_width, glxarea->pixels_height, glxarea->pixels_rowstride, NULL, NULL);
+	correct = gdk_pixbuf_flip(pixbuf, FALSE);
+
+	gdk_pixbuf_save(correct, "screenshot.png", "png", NULL, NULL);
+
+	g_object_unref(pixbuf);
+	g_object_unref(correct);
+}
+
+GType yui_gl_get_type (void) {
+  static GType yfe_type = 0;
+
+  if (!yfe_type)
+    {
+      static const GTypeInfo yfe_info =
+      {
+	sizeof (YuiGlClass),
+	NULL, /* base_init */
+        NULL, /* base_finalize */
+	(GClassInitFunc) yui_gl_class_init,
+        NULL, /* class_finalize */
+	NULL, /* class_data */
+        sizeof (YuiGl),
+	0,
+	(GInstanceInitFunc) yui_gl_init,
+      };
+
+      yfe_type = g_type_register_static(GTK_TYPE_DRAWING_AREA, "YuiGl", &yfe_info, 0);
+    }
+
+  return yfe_type;
+}
+
+static void yui_gl_class_init (YuiGlClass * klass) {
+}
+
+static void yui_gl_init (YuiGl * y) {
+	y->pixels = NULL;
+}
---
+++ tags/release_0_8_5/yabause/src/peripheral.h
@@ -0,0 +1,108 @@
+/*  Copyright 2005 Guillaume Duhamel
+    Copyright 2005-2006 Theo Berkau
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef PERIPHERAL_H
+#define PERIPHERAL_H
+
+#include "core.h"
+#include "smpc.h"
+#include "yabause.h"
+
+#define PERCORE_DEFAULT -1
+#define PERCORE_DUMMY 0
+
+extern u16 buttonbits;
+
+typedef struct
+{
+   int id;
+   const char * Name;
+   int (*Init)(void);
+   void (*DeInit)(void);
+   int (*HandleEvents)(void);
+#ifdef USENEWPERINTERFACE
+//   PerInfo_struct *(*PerGetList)(void);
+   PortData_struct *(*GetPerDataP1)(void);
+   PortData_struct *(*GetPerDataP2)(void);
+   void (*PerSetButtonMapping)();
+   u32 (*Scan)(const char *);
+   int canScan;
+   void (*Flush)(void);
+#endif
+} PerInterface_struct;
+
+extern PerInterface_struct * PERCore;
+
+typedef struct
+{
+   char *name;
+   int emulatedflags;
+   int numbuttons;
+} PerInfo_struct;
+
+extern PerInterface_struct PERDummy;
+
+int PerInit(int coreid);
+void PerDeInit(void);
+
+void PerUpPressed(void);
+void PerUpReleased(void);
+
+void PerDownPressed(void);
+void PerDownReleased(void);
+
+void PerRightPressed(void);
+void PerRightReleased(void);
+
+void PerLeftPressed(void);
+void PerLeftReleased(void);
+
+void PerStartPressed(void);
+void PerStartReleased(void);
+
+void PerAPressed(void);
+void PerAReleased(void);
+
+void PerBPressed(void);
+void PerBReleased(void);
+
+void PerCPressed(void);
+void PerCReleased(void);
+
+void PerXPressed(void);
+void PerXReleased(void);
+
+void PerYPressed(void);
+void PerYReleased(void);
+
+void PerZPressed(void);
+void PerZReleased(void);
+
+void PerRTriggerPressed(void);
+void PerRTriggerReleased(void);
+
+void PerLTriggerPressed(void);
+void PerLTriggerReleased(void);
+
+void PerKeyDown(u32);
+void PerKeyUp(u32);
+void PerSetKey(u32, const char *);
+
+#endif

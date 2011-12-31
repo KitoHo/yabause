29cda5f48062fa5545d57e8fc87bfa3a3932561e
devmiyax <devmiyax@d7be59e3-070e-4b9a-85a1-4646befae34d>
2011-12-31 03:00:28 +0000

    merge from trunk 2742-2775

--- branches/yabause-opengl/src/android/src/org/yabause/android/Yabause.java
+++ branches/yabause-opengl/src/android/src/org/yabause/android/Yabause.java
@@ -1,27 +1,54 @@
+/*  Copyright 2011 Guillaume Duhamel
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
 package org.yabause.android;
 
 import java.lang.Runnable;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import org.yabause.android.YabauseView;
 
 class YabauseRunnable implements Runnable
 {
-    public static native void init(Bitmap bitmap);
+    public static native int init(Yabause yabause, Bitmap bitmap);
     public static native void exec();
+    private boolean inited;
     private boolean paused;
     private Handler handler;
 
-    public YabauseRunnable(Bitmap bitmap)
+    public YabauseRunnable(Yabause yabause, Bitmap bitmap)
     {
         handler = new Handler();
-        init(bitmap);
+        int ok = init(yabause, bitmap);
+        inited = (ok == 0);
     }
 
     public void pause()
@@ -39,12 +66,29 @@ class YabauseRunnable implements Runnable
 
     public void run()
     {
-        if (! paused)
+        if (inited && (! paused))
         {
             exec();
             
             handler.post(this);
         }
+    }
+
+    public boolean paused()
+    {
+        return paused;
+    }
+}
+
+class YabauseHandler extends Handler {
+    private Yabause yabause;
+
+    public YabauseHandler(Yabause yabause) {
+        this.yabause = yabause;
+    }
+
+    public void handleMessage(Message msg) {
+        yabause.showDialog(msg.what, msg.getData());
     }
 }
 
@@ -52,16 +96,19 @@ public class Yabause extends Activity
 {
     private static final String TAG = "Yabause";
     private YabauseRunnable yabauseThread;
+    private YabauseHandler handler;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        YabauseView view = new YabauseView(this);
-        setContentView(view);
 
-        yabauseThread = new YabauseRunnable(view.bitmap);
+        setContentView(R.layout.main);
+
+        YabauseView view = (YabauseView) findViewById(R.id.yabause_view);
+        handler = new YabauseHandler(this);
+        yabauseThread = new YabauseRunnable(this, view.bitmap);
     }
 
     @Override
@@ -87,25 +134,70 @@ public class Yabause extends Activity
         Log.v(TAG, "this is the end...");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.emulation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.pause:
+            yabauseThread.pause();
+            return true;
+        case R.id.quit:
+            this.finish();
+            return true;
+        case R.id.resume:
+            yabauseThread.resume();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (yabauseThread.paused()) {
+            menu.setGroupVisible(R.id.paused, true);
+            menu.setGroupVisible(R.id.running, false);
+        } else {
+            menu.setGroupVisible(R.id.paused, false);
+            menu.setGroupVisible(R.id.running, true);
+        }
+        return true;
+    }
+
+    @Override
+    public Dialog onCreateDialog(int id, Bundle args) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(args.getString("message"))
+            .setCancelable(false)
+            .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Yabause.this.finish();
+                }
+            })
+            .setPositiveButton("Ignore", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+        AlertDialog alert = builder.create();
+        return alert;
+    }
+
+    private void errorMsg(String msg) {
+        Message message = handler.obtainMessage();
+        Bundle bundle = new Bundle();
+        bundle.putString("message", msg);
+        message.setData(bundle);
+        handler.sendMessage(message);
+    }
+
     static {
         System.loadLibrary("yabause");
-    }
-}
-
-class YabauseView extends View {
-    public Bitmap bitmap;
-
-    public YabauseView(Context context) {
-        super(context);
-
-        final int W = 320;
-        final int H = 240;
-
-        bitmap = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
-    }
-
-    @Override protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        invalidate();
     }
 }
